bde261eb67dcec684a1fdbf198d6c64f66cddc48
Jaime Fernández <jaime.uxd@gmail.com>
2021-10-09 19:39:57 -0500

    Styled menu added js pt 1

---
+++ script/mendel.js
@@ -0,0 +1,57 @@
+function runGregor() {
+
+  let count = 0;
+  let lha = 50;
+  let lhb = 50;
+
+  let locusA1 = 1;
+  let locusA2 = 1;
+  let locusA3 = 1;
+  let locusA4 = 1;
+  let locusA5 = 1;
+  let locusA6 = 1;
+  let locusA7 = 1;
+  let locusA8 = 1;
+  let locusA9 = 1;
+  let locusA10 = 1;
+
+  let locusB1 = 0;
+  let locusB2 = 0;
+  let locusB3 = 0;
+  let locusB4 = 0;
+  let locusB5 = 0;
+  let locusB6 = 0;
+  let locusB7 = 0;
+  let locusB8 = 0;
+  let locusB9 = 0;
+  let locusB10 = 0;
+
+  let locusC1 = 1;
+  let locusC2 = 1;
+  let locusC3 = 1;
+  let locusC4 = 1;
+  let locusC5 = 1;
+  let locusC6 = 1;
+  let locusC7 = 1;
+  let locusC8 = 1;
+  let locusC9 = 1;
+  let locusC10 = 1;
+
+  let locusD1 = 0;
+  let locusD2 = 0;
+  let locusD3 = 0;
+  let locusD4 = 0;
+  let locusD5 = 0;
+  let locusD6 = 0;
+  let locusD7 = 0;
+  let locusD8 = 0;
+  let locusD9 = 0;
+  let locusD10 = 0;
+
+  if ((locusA1 === 0)) {
+    document.getElementById('locusA1').classList.toggle('yellow');
+  } else if ((locusA1 === 1)){
+    document.getElementById('locusA1').classList.toggle('blue');
+  }
+
+}
